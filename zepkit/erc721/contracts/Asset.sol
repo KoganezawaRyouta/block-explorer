9e7dea1ca15bd72b189b62bfc08d43c5020734b9
KoganezawaRyouta <koganezawa.ryota@mf-financial.jp>
2019-06-19 20:59:24 +0900

    tes03

--- zepkit/erc721/contracts/Asset.sol
+++ zepkit/erc721/contracts/Asset.sol
@@ -4,6 +4,9 @@ pragma solidity ^0.5.0;
 import "openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol";
 
 // 2. ERC721Fullの継承
+// 1
+// 2
+// 3
 contract Asset is ERC721Full {
   constructor(string memory name, string memory symbol, uint tokenId, string memory tokenURI)
     ERC721Full(name, symbol) public {
